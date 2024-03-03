b4388b6e6c6121fdbd61dfaac7a4f3dbfba76a21
astranome <36866164+astranome@users.noreply.github.com>
2024-03-03 23:58:10 +0300

    tiny update
    try "Pause/Break" button :-)

--- usb2serial.c
+++ usb2serial.c
@@ -43,6 +43,21 @@
 
 uint8_t kbd_addr = 0;
 uint8_t kbd_inst = 0;
+/*
+const char CSI_7BIT[] = "\e[";
+const char CSI_8BIT[] = "\x9B";
+const char DCS_7BIT[] = "\eP";
+const char DCS_8BIT[] = "\x90";
+const char SS2_7BIT[] = "\eN";
+const char SS2_8BIT[] = "\x8E";
+const char SS3_7BIT[] = "\eO";
+const char SS3_8BIT[] = "\x8F";
+const char ST_7BIT[]  = "\e\\";
+const char ST_8BIT[]  = "\x9C";
+const char OSC_7BIT[] = "\e]";
+const char OSC_8BIT[] = "\x9D";
+*/
+
 
 uint8_t const hid2ascii[] = {
   0x00, 	//0-3 empty
@@ -95,57 +110,57 @@ uint8_t const hid2ascii[] = {
   '[',
   ']',
   0x5C,		// backslash
-  0x00,
+  0x5C, 		// ???
   ';',
   0x27,		//single quote
   '`',
   ',',
   '.',
   '/',
-  0x00,		//CAPS
-  0x70,		//F1
-  0x71,		//F2
-  0x72,		//F3
-  0x73,		//F4
-  0x74,		//F5
-  0x75,		//F6
-  0x76,		//F7
-  0x77,		//F8
-  0x78,		//F9
-  0x79,		//F10
-  0x00,		//F11
-  0x00,		//F12
+  0x00,		//CAPS del 04
+  'P',		//F1 3B = 58
+  'Q',		//F2
+  'R',		//F3
+  'S',		//F4
+  't',		//F5
+  'u',		//F6
+  'v',		//F7
+  'l',		//F8
+  'w',		//F9
+  'x',		//F10    1A STOPPED
+  0x6B,		//F11
+  0x6C,		//F12
   0x00,		//PrtScr
-  0x00,		//ScrlLock
-  0x00,		//Pause/Break
-  0x00,		//Ins
-  0x47,		//Home
-  0x49,		//PgUp
+  0x02,		//ScrlLock
+  0x1A,		//Pause/Break STopped ,return - fg !!!
+  0x19,		//Ins
+  0x11,		//Home
+  0x13,		//PgUp
   0x04,		//Del
-  0x00,		//End
-  0x16,		//PgDn
+  0x12,		//End
+  0x14,		//PgDn
   0x06,		//Right-Arr
   0x02,		//Left-Arr
   0x0E,		//Down-Arr
   0x10,		//Up-Arr
-  0x00,		//NUMPAD ----NumLock
-  0x00,		// /
-  0x00,		// *
-  0x00,		// -
-  0x00,		// +
-  0x00,		// ???
-  0x00,		// 1
-  0x00,		// 2
-  0x00,		// 3
-  0x00,		// 4
-  0x00,		// 5
-  0x00,		// 6
-  0x00,		// 7
-  0x00,		// 8
-  0x00,		// 9	
-  0x00,		// Ins
-  0x00,		// Del
-  '<'
+  0x01,		//NUMPAD ----NumLock but prints left<<<<<<
+  0x2F,		// /
+  0x2A,		// * (times?)
+  0x2D,		// -
+  0x2B,		// + (, = 2c)
+  0x0D,		// ??? Enter?
+  0x21,		// 1 !
+  0x22,		// 2 "
+  0x23,		// 3 #
+  0x24,		// 4 $
+  0x25,		// 5 %
+  0x26,		// 6 &
+  0x27,		// no 7 single quota '
+  0x28,		// 8 (
+  0x29,		// 9 )	
+  0x20,		// Ins prints "space"
+  0x2E,		// Del dot .2a
+  0x0D		// not Enter? (+= 2b)
 };
 
 uint8_t const hid2asciiCtrl[] = {
@@ -176,7 +191,7 @@ uint8_t const hid2asciiCtrl[] = {
   0x15,
   0x16,		//^V page down
   0x17,
-  0x18,
+  0x18,		//^X
   0x19,
   0x1A		//^Z
 };
@@ -232,9 +247,9 @@ uint8_t const hid2asciiShift[] = {
   '{',
   '}',
   '|',		// backslash
-  0x00,
+  '|',
   ':',
-  0x27,		//single quote
+  '"',		// quote mark 22h
   '~',
   '<',
   '>',
@@ -317,7 +332,9 @@ void tuh_hid_umount_cb(uint8_t dev_addr, uint8_t instance) {
 void tuh_hid_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t const* report, uint16_t len) {
 
   uint8_t ch;
+  const char fn[] = "\eO";
 
+  
   switch(tuh_hid_interface_protocol(dev_addr, instance)) {
     case HID_ITF_PROTOCOL_KEYBOARD:  
       tuh_hid_receive_report(dev_addr, instance);
@@ -331,11 +348,16 @@ void tuh_hid_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t cons
                 ch = hid2ascii[report[2]];
             } else 
                 ch = hid2ascii[report[2]];		//no Ctrl no Shift 
+		
         };
 
       #if DEBUG
-      if ((report[2]<=0x45) && (report[2]>=0x3A)) printf("%c",0);
-      if (ch) printf("%c",ch);
+      //if ((report[2]<=0x45) && (report[2]>=0x3A)) printf("%c",0);
+      if (ch) {
+	if ((report[2]<=0x45) && (report[2]>=0x3A)) printf("%s %c", fn, ch);
+	else	
+	printf("%c",ch);
+	};
       //printf("keyboard %02x %02x %02x  \n", report[0], report[1], report[2]);
       #endif
       tuh_hid_receive_report(dev_addr, instance);
